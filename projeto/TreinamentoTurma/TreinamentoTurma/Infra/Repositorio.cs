4558478d01b21563645da9a8eadf6a258bc12248
Gustavo Kuze <gustavo.silva@rcadigital.com.br>
2018-11-26 10:37:48 -0200

    Recriado o banco na RCA

--- projeto/TreinamentoTurma/TreinamentoTurma/Infra/Repositorio.cs
+++ projeto/TreinamentoTurma/TreinamentoTurma/Infra/Repositorio.cs
@@ -4,15 +4,18 @@ using System.Configuration;
 using System.Linq;
 using System.Web;
 
-namespace TreinamentoTurma.Infra
+namespace TreinamentoTurma.Infra 
 {
     public class Repositorio
     {
-        protected string ObterConnectionString { get { return ConfigurationManager.ConnectionStrings["treinamento"].ConnectionString; } }
+        protected string ObterConnectionString { get { return ConfigurationManager.ConnectionStrings["treinamento_rca"].ConnectionString; } } 
     }
 }
 
 /*
+    
+
+
     ConnectionString da RCA:
     <connectionStrings>
         <add name="treinamento" connectionString="Database=treinamento; Data Source=RCA179;Integrated Security=True"/>
