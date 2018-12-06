81857aa6c75adfe69e1a7e2c2e77103233d62e68
Gustavo Kuze <gustavo.silva@rcadigital.com.br>
2018-12-06 16:00:35 -0200

    Criada a controller auth de teste

---
+++ projeto/TreinamentoTurmaWebAPI/API/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("token")]
+        public ActionResult ObterToken()
+        {
+            string chaveDeSeguranca = "chave_de_segurança_extremamente_segura_pra_testar_o_algoritmo_06-12-2018";
+
+            var chaveSimetrica = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(chaveDeSeguranca));
+
+            var credenciaisEntrada = new SigningCredentials(chaveSimetrica, SecurityAlgorithms.HmacSha256Signature);
+
+            var token = new JwtSecurityToken(
+                issuer: "localhost",
+                audience: "localhost",
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: credenciaisEntrada
+            );
+
+            return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+        }
+    }
+}
