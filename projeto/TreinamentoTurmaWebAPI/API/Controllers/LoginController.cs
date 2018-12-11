fd900882bd5bf04bb69a8a6a0ef2bc5f26735968
Gustavo Kuze <gustavo.silva@rcadigital.com.br>
2018-12-11 16:56:19 -0200

    Estudando um maneira de gerenciar a autenticação da API manualmente

--- projeto/TreinamentoTurmaWebAPI/API/Controllers/LoginController.cs
+++ projeto/TreinamentoTurmaWebAPI/API/Controllers/LoginController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Modelos;
@@ -21,26 +22,43 @@ namespace API.Controllers
         public LoginController(ILoginServico loginServico)
         {
             _loginServico = loginServico;
-        } 
+        }
 
         [AllowAnonymous]
         [HttpPost("Autenticar")]
-        public IActionResult Autenticar([FromBody]Usuario usuario)
+        public IActionResult Autenticar([FromBody]AutenticacaoUsuario autenticacao)
         {
-            //tentando encontrar uma maneira de validar o token do usuário logado
-            var auth = new AuthenticationProperties();
-            var claims = User.Claims.Where(x => x.Value == usuario.Id.ToString());
+            /*
+             O que posso fazer aqui é chegar se o token da autenticação passada
+             por parametro não é vazio, e se está válido (ainda não expirou)
+             talvez verificar se o usuário atual é o usuário do token
 
-            var identities = User.Identities;
+            caso nada disso esteja válidado devidamente, envia pra autenticação, do contrário 
+            damos um jeito de salvar o usuário do token na memória, ou usamos a própria claim da prop User
+            e retornamos o token novo/atual para o client
+             */
+
+            var tok = autenticacao.Token;
+            var tokHandler = new JwtSecurityTokenHandler();
+            if (tok != string.Empty && tokHandler.CanReadToken(tok))
+            {
+                var token = tokHandler.ReadJwtToken(tok);
+                if( token.ValidTo.CompareTo(DateTime.UtcNow) > 0)
+                {
+                    // aqui o token expirou no caso
+                }
+            }
+
+            //var claims = User.Claims.Where(x => x.Value == autenticacao.Usuario.Id.ToString());
             
-            var login = _loginServico.Autenticar(usuario.Codigo, usuario.Senha);
+            var login = _loginServico.Autenticar(autenticacao.Usuario.Codigo, autenticacao.Usuario.Senha);
 
             if (login == null)
                 return BadRequest(new { message = "Codigo ou senha incorretos" });
 
             return Ok(login);
         }
-        
+
         [HttpGet("teste")]
         public ActionResult Teste()
         {
